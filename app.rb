ef32e0bdf1265543acb3fe69b202105a7be89bd8
Sara Walker <sarawall1187@gmail.com>
2019-03-11 14:52:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/' do
-   @analyzed_text = TextAnalyzer.new(params[:user_text])
+   @analyzed_text.text = TextAnalyzer.new(params[:user_text])
    erb :results
    binding.pry
   end
